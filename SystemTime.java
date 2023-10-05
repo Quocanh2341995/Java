ebf87580b5d667d9a4f2df6bfd61ce983f552ee5
Quocanh2341995 <143682635+Quocanh2341995@users.noreply.github.com>
2023-10-05 16:20:03 +0700

    Add files via upload

---
+++ SystemTime.java
@@ -0,0 +1,19 @@
+import java.util.Date;
+
+//public class SystemTime {
+//    public static void main(String[] args) {
+//        Date now = new Date();
+//        System.out.println("Now is :" + now);
+//    }
+//}
+
+public class main {
+    public static void main(int[] arr) {
+        int[] arr = {1,2,3,4};
+    }
+
+    public static void upelement(int[] arr){
+        int index = 0;
+        for (int i = 0; i < arr.length; i++);
+    }
+}
