c344033519a327aa2fc609bb62cae701f3701423
Quocanh2341995 <143682635+Quocanh2341995@users.noreply.github.com>
2023-10-10 16:53:59 +0700

    Add files via upload

--- Rectangle.java
+++ Rectangle.java
@@ -2,16 +2,37 @@ import java.util.Scanner;
 
 public class Rectangle {
     public static void main(String[] args) {
-        float width;
-        float height;
-
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter width :");
-        width = scanner.nextFloat();
-        System.out.println("Enter height ");
-        height = scanner.nextFloat();
-
-        float area = width * height;
-        System.out.println("Area is :" + area);
+        System.out.print("Enter the width: ");
+        double width = scanner.nextDouble();
+        System.out.print("Enter the height: ");
+        double height = scanner.nextDouble();
+        Rectangle rectangle = new Rectangle(width, height);
+        System.out.println("Your rectangle \n" + rectangle.display());
+        System.out.println("Perimeter of the Rectangle: " + rectangle.getPerimeter());
+        System.out.println("Area of the Rectangle: " + rectangle.getArea());
     }
+
+    double width, height;
+    public Rectangle() {
+    }
+
+    public Rectangle (double width, double height){
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getArea(){
+        return this.width * this.height;
+    }
+
+    public double getPerimeter(){
+        return (this.width + this.height) * 2;
+    }
+
+    public String display(){
+        return "Rectangle{" + "width" + width + ", height=" + height + "}" ;
+    }
+
+
 }
